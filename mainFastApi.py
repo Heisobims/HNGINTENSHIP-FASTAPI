ee81f551fdf0f26c0f150996b9f7c59516228354
heisobims <heisobims@gmail.com>
2025-01-29 14:56:27 +0100

    Add FastAPI application with CORS middleware and userinfo endpoint

---
+++ mainFastApi.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from datetime import datetime
+import pytz
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/userinfo")
+def get_info():
+    return {
+        "email": "heyobims@gmail.com",
+        "current_datetime": datetime.now(pytz.utc).isoformat(),
+        "github_url": "https://github.com/heisobims/HNGINTENSHIP-FASTAPI"
+    }
+    
